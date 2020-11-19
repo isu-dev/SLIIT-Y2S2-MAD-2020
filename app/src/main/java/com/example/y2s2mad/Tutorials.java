9ddecf92366d5540118e918d33fffd5b80811145
isu-dev <arachchigeisuridevlini>
2020-11-19 16:23:35 +0530

    Tutorial 4 - Image Handling

--- app/src/main/java/com/example/y2s2mad/Tutorials.java
+++ app/src/main/java/com/example/y2s2mad/Tutorials.java
@@ -2,13 +2,30 @@ package com.example.y2s2mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.y2s2mad.All_Tutorials.T4_ImageHandling;
 
 public class Tutorials extends AppCompatActivity {
+
+    Button btn_tute4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorials);
+
+        btn_tute4 = findViewById(R.id.btn_tute4);
+
+        btn_tute4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Tutorials.this, T4_ImageHandling.class);
+                startActivity(intent);
+            }
+        });
     }
 }
